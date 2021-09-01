df250160fb048903ed15cbf549aee5adda3a0929
AV <vihrogonov@protonmail.com>
2021-09-01 19:18:28 +0100

    React State & Working with Events

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-function App() {
+import React from 'react';
+
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
+  const expenses = [
+    {
+      id: 1,
+      title: 'Car Insurance',
+      amount: 298.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 2,
+      title: 'Home Insurance',
+      amount: 50.55,
+      date: new Date(2021, 9, 22),
+    },
+    {
+      id: 3,
+      title: 'Live Insurance',
+      amount: 29,
+      date: new Date(2021, 11, 8),
+    },
+  ];
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense />
+      <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
